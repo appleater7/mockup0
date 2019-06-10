f036e99a093ec58d65514cc2ef4f5c70f6ec85e4
appleater <appleater7@gmail.com>
2019-06-10 09:50:30 +0900

    ....

---
+++ src/main/java/com/mockup0/service/MockUpService.java
@@ -0,0 +1,5 @@
+package com.mockup0.service;
+
+public interface MockUpService {
+
+}
---
+++ src/main/java/com/mockup0/service/impl/MockUpServiceImpl.java
@@ -0,0 +1,10 @@
+package com.mockup0.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.mockup0.service.MockUpService;
+
+@Service
+public class MockUpServiceImpl implements MockUpService {
+
+}
